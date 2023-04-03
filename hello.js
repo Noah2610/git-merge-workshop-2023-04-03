81c659105eac55dc48aca4bc31fbccf64f02783d
Noah Rosenzweig <noah.rosenzweig@codecool.com>
2023-04-03 13:56:00 +0200

    Update loop

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 function sayHello() {
-    for (let i = 5; i < 20; i++) {
-        console.log("6 - 20: " + i + 1);
+    for (let i = 3; i < 10; i++) {
+        console.log("num: " + i + 1);
     }
 }
 
